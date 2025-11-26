71120a1a6b778aa9dac393647374c06a2833421d
rizkurniawan <rizkykurniawan2023@gmail.com>
2025-11-26 14:37:33 +0700

    selesai develop and testing

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -4,7 +4,7 @@ public class AplikasiTodolist {
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
 
     public static void main(String[] args) {
-        testViewRemoveTodolist();
+        viewShowTodoList();
     }
 
     /**
